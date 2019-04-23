78bbfbb8fa1755cbc44f99c3df6e7b3618c92ba0
Anna Beagley <beagley.dev@gmail.com>
2019-04-23 10:27:01 -0600

    initial commit

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,51 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
+
+async function signup(parent, args, context, info) {
+    const password = await bcrypt.hash(args.password, 10)
+
+    const user = await context.prisma.createUser({ ...args, password })
+
+    const token = jwt.sign({ userId: user.id}, APP_SECRET)
+
+    return {
+        token,
+        user,
+    }
+}
+
+function post(parents, args, context, info) {
+    const userId = getUserId(context)
+    return context.prisma.createLink({
+        url: args.url,
+        description: args.description,
+        postedBy: { connect: { id: userId } },
+    })
+}
+
+async function login(parent, args, context, info) {
+    const user = await context.prisma.user({ email: args.email })
+    if (!user) {
+        throw new Error('No such user found')
+    }
+
+    const valid = await bcrypt.compar(args.password, user.password)
+    if (!valid) {
+        throw new Error('Invalid password')
+    }
+
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+    return {
+        token,
+        user,
+    }
+}
+
+module.exports = {
+    signup,
+    login,
+    post,
+}
